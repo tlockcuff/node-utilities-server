c6cd8c90d34a2fc8ff0040cbf2057659195b76d4
Matt Fetterman <navx2810@gmail.com>
2017-10-25 21:20:45 -0400

    Changed the screenshot's downloaded filename
    Added in a header which sets the filename when someone right-clicks and saves the file.
    [hostname]_[length]x[width]_screenshot_[time].jpg

--- routes/screenshot/index.ts
+++ routes/screenshot/index.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import * as puppeteer from 'puppeteer';
 import {resolve, join} from 'path';
 import * as fs from 'fs';
+import { parse } from "url"
 
 const screenshotPath = resolve('./temp/screenshots/');
 
@@ -10,13 +11,23 @@ function screenshot(req: Request, res: Response){
     var height = (req.query.h) ? parseInt(req.query.w) : 1200;
     var width = (req.query.w) ? parseInt(req.query.w) : 1920;
 
+    let opts = {
+        headers: {
+            "Content-Disposition": "inline; "
+        }
+    }
+
+    const parsedUrl = parse(url)
+
     puppeteer.launch().then(function(browser){
         browser.newPage().then(function(page){
             page.goto(url).then(function(loadedPage){
                 page.setViewport({ height: height, width: width });
+                const fname = `${parsedUrl ? `${parsedUrl.hostname}_` : ''}${width}x${height}_screenshot_${Date.now()}.jpg`
+                opts.headers["Content-Disposition"] += `filename=${fname};`
                 var filePath = join(screenshotPath, `screenshot_${Date.now()}.jpg`);
                 page.screenshot({ fullPage: true, path : filePath, quality:50 }).then(function(file){
-                    res.sendFile(filePath);
+                    res.sendFile(filePath, opts);
                     browser.close();
                 });
             });
