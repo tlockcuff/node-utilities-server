25e1cbaa23c8e0047f289f947ee0a751aa71f309
Matt Fetterman <navx2810@gmail.com>
2017-11-05 20:33:53 -0500

    Started changing logic to handle project naming.
    Removed the init function that cleared the test folder on restart.
    Started writing notes to convert the extraction and creation process to include the project ID handed over.
    Started writing notes for creating a meta.json file that stores the previous test metadata for the project.

--- routes/testing/index.route.ts
+++ routes/testing/index.route.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import Socket from "./IO"
 import { resolve, join, basename } from "path"
-import { existsSync } from "fs"
+import { existsSync, writeFile, readFile } from "fs"
 import * as multer from "multer"
 import * as v4 from "uuid/v4"
 import * as AdmZip from "adm-zip"
@@ -14,6 +14,7 @@ let TEMP_DIR: TempDir
 const testUploader = multer({
     storage: multer.diskStorage({
         filename: (req, file, cb) => {
+            // todo: transform the filename to be the project id instead of a generated uuid
             cb(null, v4() + ".zip")
         },
         destination: (req, file, cb) => {
@@ -43,21 +44,20 @@ export function get(req: Request, res: Response) {
         } else {
             res.status(400).json({error: `Test archive ${id} does not exist.`})
         }
+    } else if(id) {
+        // check if file exists
+            // if it does, return the json data from the meta.json in the test folder
     }
 }
 
-export async function init() {
-    const path = './temp/tests'
-    await cleanDirectory(path)
-    TEMP_DIR = new TempDir(path)
-}
-
 Socket.rooms.TestingSuite.on('connection', (socket) => {
-    socket.on('test', async function ({ uuid }) {
+    socket.on('test', async function ({ id }) {
+        let results = []    // JSON array storing results to write to a file.
+
         this.emit('status', 'Extracting.')
-        const zipPath = join(TEMP_DIR.tmpPath, uuid)
-        if( !existsSync(zipPath) ) { this.emit('status', `Test ${uuid} does not exist.`) }
-        const path = join(TEMP_DIR.path, uuid)
+        const zipPath = join(TEMP_DIR.tmpPath, id)
+        if( !existsSync(zipPath) ) { this.emit('status', `Test ${id} does not exist.`) }
+        const path = join(TEMP_DIR.path, id)
         console.log(`extracting to ${path}`)
         const zip = new AdmZip(`${zipPath}.zip`)
         // todo: ADMZip can extract to memory instead of to the file directory
@@ -73,13 +73,26 @@ Socket.rooms.TestingSuite.on('connection', (socket) => {
             proc.stderr.pipe(process.stderr)
             proc.stdout.setEncoding("utf-8")
             proc.stdout.on('data', (d) => {
+                d = JSON.stringify(d)
+                results.push(d)
                 this.emit('result', d)
             })
             proc.on('close', (code, sig) => {
                 // code returns the number of failures instead of an exit code.
                 this.emit('done', code)
+                let test = {
+                    status: { passed: 0, failed: 0 },
+                    results: results
+                }
+                // calculate number of passes and fails.
+                // write the test object to a meta.json file in the test/{id} folder.
+                console.log(`writing the meta.json file to the test ${id}`)
                 console.log(`mocha is finished. code: ${code}, SIG: ${sig}`)
             })
         })
     })
 })
+
+function ReadTestData(id: string) {
+
+}
